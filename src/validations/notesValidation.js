e79380601780e0e590146b28dd7b2997a432f4ec
Olena Kolesnik <lelya6350@gmail.com>
2026-05-18 18:56:22 +0300

    fixed hw

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,12 +1,12 @@
 import { Segments, Joi } from "celebrate";
 import { isValidObjectId } from "mongoose";
-import { tags } from "../constants/tags.js";
+import { TAGS } from "../constants/tags.js";
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...tags),
+    tag: Joi.string().valid(...TAGS).trim(),
     search: Joi.string().trim().allow(''),
   }),
 };
@@ -15,7 +15,7 @@ export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
-    tag: Joi.string().valid(...tags),
+    tag: Joi.string().valid(...TAGS).trim().default('Todo'),
   }),
 };
 
@@ -36,7 +36,7 @@ export const updateNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
     content: Joi.string().allow(''),
-    tag: Joi.string().valid(...tags),
+    tag: Joi.string().valid(...TAGS).trim().default('Todo'),
   }).min(1),
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(objectIdValidator).required(),
