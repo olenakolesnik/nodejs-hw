a86ff998bce151a596567d77fd067babf999f7bb
Olena Kolesnik <lelya6350@gmail.com>
2026-05-21 23:24:11 +0300

    fixed hw-auth

--- src/models/note.js
+++ src/models/note.js
@@ -26,5 +26,5 @@ const noteSchema = new Schema({
 }, {
   timestamps: true,
 });
-noteSchema.index({ tag: 1 });
+noteSchema.index({ tag: 1, userId: 1 });
 export const Note = model('Note', noteSchema);
