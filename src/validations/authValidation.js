65308765b8748fa606f6d90c01d09f85455321b5
Olena Kolesnik <lelya6350@gmail.com>
2026-05-27 21:21:54 +0300

    made task2

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -12,3 +12,14 @@ export const loginUserSchema = {
     password: Joi.string().required(),
   }),
 };
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+};
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    token: Joi.string().required(),
+    password: Joi.string().min(8).required(),
+  }),
+};
